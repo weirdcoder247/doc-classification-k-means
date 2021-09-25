dfd0bd79b12231e765617c50bccd3c294a2fe989
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2021-09-25 13:45:10 +0530

    initial commit with basic code

---
+++ 01_codes/k-means.py
@@ -0,0 +1,11 @@
+# import packages
+import pandas as pd
+import numpy as np
+import os
+
+# Set working directory
+os.getcwd()
+os.chdir("C:\\Repositories\\doc-classification-k-means\\02_data")
+
+# import data
+data = pd.read_csv("abcnews-date-text.csv")
